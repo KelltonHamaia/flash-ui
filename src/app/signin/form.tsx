48e9324f308db8f0821ed9355dac1deceeecf630
kellton_hamaia <hamaia.kell@gmail.com>
2024-12-16 20:03:48 -0300

    :art: style: Adicionar bordas quando os inputs receberem foco

--- src/app/signin/form.tsx
+++ src/app/signin/form.tsx
@@ -8,24 +8,24 @@ import Link from "next/link"
 
 export const SignInForm = () => {
     return (
-        <div className="w-full h-80 lg:h-[540px] flex-1 col-span-1 lg:col-span-2">
+        <div className="w-full h-96 lg:h-[540px] flex-1 col-span-1 lg:col-span-2">
             <div className="flex flex-col w-full h-full gap-2 p-5 border rounded lg:gap-5 border-zinc-700">
-                <Label htmlFor="email" className="ml-2 text-lg font-semibold">Seu email</Label>
+                <Label htmlFor="email" className="ml-2 text-base font-semibold md:text-lg">Seu email</Label>
                 <div className="flex items-end">
                     <ArrowRight className="hidden md:block" />
                     <Input
                         id="email"
                         type="email"
-                        className="h-5 border-transparent rounded-none outline-none border-b-zinc-700 focus:ring-0 focus-visible:ring-transparent"
+                        className="h-5 border-transparent rounded-none outline-none focus:border-b-indigo-700 border-b-zinc-700 focus:ring-0 focus-visible:ring-transparent"
                     />
                 </div>
-                <Label htmlFor="password" className="ml-2 text-lg font-semibold">Sua senha</Label>
+                <Label htmlFor="password" className="ml-2 text-base font-semibold md:text-lg">Sua senha</Label>
                 <div className="flex items-end">
                     <ArrowRight className="hidden md:block" />
                     <Input
                         id="password"
                         type="password"
-                        className="h-5 border-transparent rounded-none outline-none border-b-zinc-700 focus:ring-0 focus-visible:ring-transparent"
+                        className="h-5 border-transparent rounded-none outline-none focus:border-b-indigo-700 border-b-zinc-700 focus:ring-0 focus-visible:ring-transparent"
                     />
                 </div>
 
@@ -35,7 +35,7 @@ export const SignInForm = () => {
 
                 <hr />
 
-                <div className="flex flex-col md:flex-row md:justify-between">
+                <div className="flex flex-col gap-4 mt-5 lg:mt-0 lg:gap-0 md:flex-row md:justify-between">
                     <Button variant={"link"} asChild className="h-5">
                         <Link href={"/signup"} className="text-xs tracking-tighter">Não tenho uma conta 😬</Link>
                     </Button>
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -4,9 +4,9 @@ import { SignInForm } from "./form";
 const Page = () => {
 
     return (
-        <div className='container grid max-w-5xl min-h-screen grid-cols-1 gap-5 p-5 mx-auto lg:grid-cols-5 lg:flex-row lg:items-center'>
-            <div className="flex flex-col gap-5 lg:gap-10 w-full h-80 md:h-60 lg:h-[540px] px-5 md:border-none border rounded border-zinc-700 col-span-1 lg:col-span-3">
-                <h1 className="text-2xl font-semibold lg:text-3x">{phrases[new Date().getDay() - 1].phrase}</h1>
+        <div className='container grid max-w-5xl min-h-screen grid-cols-1 gap-5 p-5 mx-auto tracking-tighter lg:grid-cols-5 lg:flex-row lg:items-center'>
+            <div className="flex flex-col gap-5 lg:gap-10 w-full h-96 pt-5 md:pt-0 md:h-60 lg:h-[540px] px-5 md:border-none border rounded border-zinc-700 col-span-1 lg:col-span-3">
+                <h1 className="text-2xl font-semibold lg:text-4xl">{phrases[new Date().getDay() - 1].phrase}</h1>
                 <p className="text-lg font-light md:text-2xl md:text-start">
                     Por favor, informe seus dados <br />
                     e vamos estudar um <br /> pouquinho!

de089ee732d6c3a90280013f3cfc595e31f66638
kellton_hamaia <hamaia.kell@gmail.com>
2024-12-16 19:23:08 -0300

    :sparkles: feat: Criar página de login
    - Adições e mudanças
     - [/Components] -> Adicionar componente Input e Label @shadcn

---
+++ src/_data/signin-phrases.ts
@@ -0,0 +1,9 @@
+export const phrases = [
+    { phrase: "Opa!! Que massa te ver aqui nessa segunda! Vamos estudar?" },
+    { phrase: "Terça-feira, um dia bom para estudar! Bora?" },
+    { phrase: "Enfim quarta, meio da semana! Falta só mais um pouco pro nosso sextou!" },
+    { phrase: "Estamos quase sextando! Vamo vamo vamo, estude!!!" },
+    { phrase: "Finalmente sextou! Estude e aproveite seu final de semana!" },
+    { phrase: "Caramba! Você aqui no sábado?? Que dedicação! Vamos estudar!" },
+    { phrase: "Não acredito que no domingão você está aqui estudando! Parabéns!" },
+];
---
+++ src/app/signin/form.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@radix-ui/react-label"
+import { ArrowRight } from "lucide-react"
+import Link from "next/link"
+
+export const SignInForm = () => {
+    return (
+        <div className="w-full h-80 lg:h-[540px] flex-1 col-span-1 lg:col-span-2">
+            <div className="flex flex-col w-full h-full gap-2 p-5 border rounded lg:gap-5 border-zinc-700">
+                <Label htmlFor="email" className="ml-2 text-lg font-semibold">Seu email</Label>
+                <div className="flex items-end">
+                    <ArrowRight className="hidden md:block" />
+                    <Input
+                        id="email"
+                        type="email"
+                        className="h-5 border-transparent rounded-none outline-none border-b-zinc-700 focus:ring-0 focus-visible:ring-transparent"
+                    />
+                </div>
+                <Label htmlFor="password" className="ml-2 text-lg font-semibold">Sua senha</Label>
+                <div className="flex items-end">
+                    <ArrowRight className="hidden md:block" />
+                    <Input
+                        id="password"
+                        type="password"
+                        className="h-5 border-transparent rounded-none outline-none border-b-zinc-700 focus:ring-0 focus-visible:ring-transparent"
+                    />
+                </div>
+
+                <Button className="w-full my-5 text-white rounded md:mb-10 lg:px-4 lg:py-2 bg-rose-700 hover:bg-rose-800">
+                    Vamos Lá!
+                </Button>
+
+                <hr />
+
+                <div className="flex flex-col md:flex-row md:justify-between">
+                    <Button variant={"link"} asChild className="h-5">
+                        <Link href={"/signup"} className="text-xs tracking-tighter">Não tenho uma conta 😬</Link>
+                    </Button>
+                    <Button variant={"link"} asChild className="h-5">
+                        <Link href={"/forgotpassword"} className="text-xs tracking-tighter">Esqueci minha senha 😓</Link>
+                    </Button>
+                </div>
+
+            </div>
+        </div>
+    )
+}

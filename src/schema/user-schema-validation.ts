761c41a0b17a3e770b901c990dbd886e4e7eb7c6
kellton_hamaia <hamaia.kell@gmail.com>
2024-12-17 00:16:50 -0300

    :sparkles: feat: Adicionar schemas de validação para users

---
+++ src/schema/user-schema-validation.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+
+const requiredError = {
+    required_error: "Este campo é obrigatório.",
+}
+
+export const validateUserData = z.object({
+    name: z.string(requiredError).min(2, "Nome deve ter no mínimo 2 caracteres"),
+    email: z.string(requiredError).email("Informe um email válido"),
+    password: z.string(requiredError).min(6, "Senha deve ter no mínimo 6 caracteres"),
+    confirmPassword: z.string(requiredError).min(6, "Senha deve ter no mínimo 6 caracteres"),
+})
+.refine( keys => keys.password === keys.confirmPassword, {
+    message: "As senhas não conferem.",
+    path: ["confirmPassword"]
+});
+
+export const validateUserEntries = z.object({
+    email: z.string(requiredError).email("Informe um email válido"),
+    password: z.string(requiredError),
+});
+
+export type NewUserdata = z.infer<typeof validateUserData>;
+
+export const updateUserData = z.object({
+    name: z.string(requiredError).min(2, "Nome deve ter no mínimo 2 caracteres").optional(),
+    email: z.string(requiredError).email("Informe um email válido").optional(),
+    avatar: z.string(requiredError).optional(),
+    password: z.string(requiredError).min(6, "Senha deve ter no mínimo 6 caracteres").optional(),
+    confirmPassword: z.string(requiredError).min(6, "Senha deve ter no mínimo 6 caracteres").optional(),
+})
+.refine( keys => keys.password === keys.confirmPassword, {
+    message: "As senhas não conferem.",
+    path: ["confirmPassword"]
+});

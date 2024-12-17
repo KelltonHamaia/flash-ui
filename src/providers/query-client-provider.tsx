8939d77aab6bce3f5a36bc800e1be8356e571967
kellton_hamaia <hamaia.kell@gmail.com>
2024-12-17 00:08:37 -0300

    :sparkles: feat: Criar provider do @tanstack/react-query
    - Adições e mudanças
     - Criação do queryClient

---
+++ src/providers/query-client-provider.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { queryClient } from "@/providers/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode
+}
+
+export const GlobalQueryClient = ({ children }: Props) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+            <ReactQueryDevtools
+                initialIsOpen={false}
+                buttonPosition="bottom-right"
+                position="right"
+            />
+        </QueryClientProvider>
+    )
+}
